fd162b5f6a8e7c1e60d785823637cc37750877c3
puertasman <puertasman@gmail.com>
2025-03-31 15:28:51 +0200

    cambios en el juego

---
+++ 002-juego_logica/assets/js/preguntas.js
@@ -0,0 +1,20 @@
+const preguntas = [
+    {
+        categoria: "🔢 Números",
+        pregunta: "¿Cuánto es 2 + 2?",
+        respuestas: ["3", "4", "5", "6"],
+        correcta: 1
+    },
+    {
+        categoria: "🎨 Formas / colores",
+        pregunta: "¿De qué color es el cielo?",
+        respuestas: ["Rojo", "Azul", "Verde", "Amarillo"],
+        correcta: 1
+    },
+    {
+        categoria: "🔀 Mixtos",
+        pregunta: "¿Cuál es el animal más rápido?",
+        respuestas: ["Tigre", "Guepardo", "León", "Elefante"],
+        correcta: 1
+    }
+]
